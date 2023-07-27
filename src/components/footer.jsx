135301e72c810fb189fb5bad8f48448820ae06bc
Utkarsh Nigam <webxthedev@gmail.com>
2023-07-28 00:23:03 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -18,9 +18,6 @@ const footer = () => {
               <a href="/en/team" className="text-gray-400 hover:text-white">Team</a>
             </li>
             <li>
-              <a href="/en/bots" className="text-gray-400 hover:text-white">Bots</a>
-            </li>
-            <li>
               <a href="/en/pricing" className="text-gray-400 hover:text-white">Premium</a>
             </li>           
             
