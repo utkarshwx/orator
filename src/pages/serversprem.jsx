9891724b95cde1d444e723d90df78a5a6fc4dc36
Utkarsh Nigam <webxthedev@gmail.com>
2023-07-28 01:07:04 +0530

    Add files via upload

---
+++ src/pages/serversprem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "../components/header.jsx";
+import Footer from "../components/footer.jsx";
+import Serverpre from "../components/serverpre.jsx";
+import Curve2 from "../components/curve2.jsx";
+
+const serversprem = () => {
+  return (
+    <div>
+        <Header/>
+        <Serverpre/>
+        <Curve2/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default serversprem
