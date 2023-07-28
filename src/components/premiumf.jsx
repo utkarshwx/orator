7a074c20bd85263d93ddba21e42d716ab25a0153
Utkarsh Nigam <webxthedev@gmail.com>
2023-07-29 00:10:57 +0530

    Update premiumf.jsx

--- src/components/premiumf.jsx
+++ src/components/premiumf.jsx
@@ -3,72 +3,72 @@ import React from "react";
 const premiumf = () => {
   return (
     <div>
-      <section class="text-gray-400 body-font">
-        <div class="container px-14 pt-14 pb-10 mx-auto flex-wrap items-center">
-          <div class="flex flex-col text-center w-full mb-20">
+      <section className="text-gray-400 body-font">
+        <div className="container px-14 pt-14 pb-10 mx-auto flex-wrap items-center">
+          <div className="flex flex-col text-center w-full mb-20">
             
-            <h1 class="sm:text-5xl text-3xl font-medium title-font text-white">
+            <h1 className="sm:text-5xl text-3xl font-medium title-font text-white">
               Premium Features
             </h1>
           </div>
-          <div class="conatiner flex-wrap flex -m-4 items-center justify-center text-center">
-            <div class="p-4 md:w-1/3">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-4 sm:p-8 flex-col justify-center">
-                <div class="w-80 sm:flex items-center m-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+          <div className="conatiner flex-wrap flex -m-4 items-center justify-center text-center">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-4 sm:p-8 flex-col justify-center">
+                <div className="w-80 sm:flex items-center m-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                     </svg>
                   </div>
-                  <h2 class="text-white text-base sm:text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-base sm:text-lg title-font font-medium mt-2 sm:mt-0">
                   Panel & Hooks Panel
                   </h2>
                 </div>
                 
               </div>
             </div>
-            <div class="p-4 md:w-1/3">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col justify-center">
-                <div class="w-80 sm:flex items-center mb-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col justify-center">
+                <div className="w-80 sm:flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                       <circle cx="12" cy="7" r="4"></circle>
                     </svg>
                   </div>
-                  <h2 class="text-white text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-lg title-font font-medium mt-2 sm:mt-0">
                   Access to all 3 voice APIs.
                   </h2>
                 </div>                
               </div>
             </div>
-            <div class="p-4 md:w-1/3 ">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
-                <div class="w-80 sm:flex items-center mb-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <div className="p-4 md:w-1/3 ">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="w-80 sm:flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <circle cx="6" cy="6" r="3"></circle>
@@ -76,70 +76,70 @@ const premiumf = () => {
                       <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                     </svg>
                   </div>
-                  <h2 class="text-white text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-lg title-font font-medium mt-2 sm:mt-0">
                   A Premium Role in Support Server.
                   </h2>
                 </div>
               </div>
             </div>
           
-            <div class="p-4 md:w-1/3">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
-                <div class="w-80 sm:flex items-center mb-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="w-80 sm:flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                     </svg>
                   </div>
-                  <h2 class="text-white text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-lg title-font font-medium mt-2 sm:mt-0">
                   TTS Logging.
                   </h2>
                 </div>
               </div>
             </div>
-            <div class="p-4 md:w-1/3">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
-                <div class="w-80 sm:flex items-center mb-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="w-80 sm:flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                       <circle cx="12" cy="7" r="4"></circle>
                     </svg>
                   </div>
-                  <h2 class="text-white text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-lg title-font font-medium mt-2 sm:mt-0">
                   Beta Access
                   </h2>
                 </div>
                 
               </div>
             </div>
-            <div class="p-4  md:w-1/3">
-              <div class="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 sm:p-8 flex-col justify-center">
-                <div class="w-80 sm:flex items-center mb-3">
-                  <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <div className="p-4  md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 sm:p-8 flex-col justify-center">
+                <div className="w-80 sm:flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
                     <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       stroke-width="2"
-                      class="w-5 h-5"
+                      className="w-5 h-5"
                       viewBox="0 0 24 24"
                     >
                       <circle cx="6" cy="6" r="3"></circle>
@@ -147,7 +147,7 @@ const premiumf = () => {
                       <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                     </svg>
                   </div>
-                  <h2 class="text-white text-lg title-font font-medium mt-2 sm:mt-0">
+                  <h2 className="text-white text-lg title-font font-medium mt-2 sm:mt-0">
                   Whitelisting & Blacklisting support with more customisation.
                   </h2>
                 </div>
